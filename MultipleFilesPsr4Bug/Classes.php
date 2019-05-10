eb5ad4b147ac5b3e4ee5daffc2826b6400e78b81
Jan Mikeš <j.mikes@me.com>
2019-05-10 22:30:36 +0200

    Namespaces

--- MultipleFilesPsr4Bug/Classes.php
+++ MultipleFilesPsr4Bug/Classes.php
@@ -1,5 +1,7 @@
 <?php declare (strict_types=1);
 
+namespace PSR4Bug;
+
 final class ClassOne {}
 
 final class ClassTwo {}
--- MultipleFilesPsr4Bug/Grid.php
+++ MultipleFilesPsr4Bug/Grid.php
@@ -1,5 +1,6 @@
 <?php declare (strict_types=1);
 
+namespace PSR4Bug;
 
 final class Grid
 {
